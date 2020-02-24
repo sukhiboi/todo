3c38d5ac61b51da3d2973e064abbc1e5ef8a1fc3
sukhiboi <41sukhdevsingh@gmail.com>
2020-02-24 16:37:49 +0530

    Using Redis

--- config.js
+++
@@ -1,5 +0,0 @@
-const DATA_STORE_PATH =
-  process.env.DATA_STORE_PATH || `${__dirname}/data/datastore.json`;
-const USERS_PATH = process.env.USERS_PATH || `${__dirname}/data/users.json`;
-DATA_STORAGE_PATH = process.env.DATA_STORE_PATH || `${__dirname}/data`;
-module.exports = { DATA_STORE_PATH, USERS_PATH, DATA_STORAGE_PATH };
